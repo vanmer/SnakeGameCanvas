6bb98f5545dfa789130a90c31ac8ef00ea1e9fd0
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-29 12:45:02 +0200

    load audio files

--- snake.js
+++ snake.js
@@ -11,6 +11,21 @@ ground.src = "img/ground.png";
 const foodImg = new Image();
 foodImg.src = "img/food.png";
 
+// load audio files
+const dead = new Audio();
+const eat = new Audio();
+const left = new Audio();
+const up = new Audio();
+const right = new Audio();
+const down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+left.src = "audio/left.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+down.src = "audio/down.mp3";
+
 // create the snake
 let snake = [];
 snake[0] = {
